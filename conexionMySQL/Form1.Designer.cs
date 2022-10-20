6e7a054ec3b02af9745cebdd8c8b97389ab8781b
CarlosArturoGT <116294035+CarlosArturoGT@users.noreply.github.com>
2022-10-20 10:50:03 -0500

    commit  inicial
    carga inicial de proyecto para alumnos de  Licenciatura en Tecnologías de la Información , especializante en base de datos

---
+++ conexionMySQL/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace conexionMySQL
+{
+    partial class conexion_MySql
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.txtcontrasena = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // conectar
+            // 
+            this.conectar.Location = new System.Drawing.Point(259, 158);
+            this.conectar.Name = "conectar";
+            this.conectar.Size = new System.Drawing.Size(202, 49);
+            this.conectar.TabIndex = 0;
+            this.conectar.Text = "Conectar";
+            this.conectar.UseVisualStyleBackColor = true;
+            this.conectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contraseña:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Host:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(144, 28);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(204, 20);
+            this.txtHost.TabIndex = 4;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(144, 72);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(204, 20);
+            this.txtusuario.TabIndex = 5;
+            // 
+            // txtcontrasena
+            // 
+            this.txtcontrasena.Location = new System.Drawing.Point(144, 115);
+            this.txtcontrasena.Name = "txtcontrasena";
+            this.txtcontrasena.Size = new System.Drawing.Size(204, 20);
+            this.txtcontrasena.TabIndex = 6;
+            // 
+            // conexion_MySql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(473, 221);
+            this.Controls.Add(this.txtcontrasena);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.conectar);
+            this.Name = "conexion_MySql";
+            this.Text = "Conexion_MySql";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.TextBox txtcontrasena;
+    }
+}
+
---
+++ conexionMySQL/Form1.cs
@@ -0,0 +1,73 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conexionMySQL
+{
+    public partial class conexion_MySql : Form
+    {
+    private  MySqlConnection conexion; 
+
+        public conexion_MySql()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtHost.Text))
+            {
+                MessageBox.Show("El HOST es un campo requerido");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtusuario.Text))
+            {
+                MessageBox.Show("El USUARIO es un campo requerido");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtcontrasena.Text))
+            {
+                MessageBox.Show("La CONTRASEÑA  es un campo requerido");
+                return;
+            }
+
+            try
+            {
+
+                conexion = new MySqlConnection();
+                conexion.ConnectionString =
+                    "server=" + txtHost.Text + ";"
+                    + "user id= " + txtusuario.Text + ";"
+                    + "password=" + txtcontrasena.Text + ";";
+                conexion.Open();
+                MessageBox.Show("LA CONEXION SE REALIZO EXITOSAMENTE ");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Hubo un erro al conectar:" + ex.Message);
+            }
+            finally
+            {
+                if (conexion.State == System.Data.ConnectionState.Open)
+                    conexion.Close();
+            }
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
